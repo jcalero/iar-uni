c97c2e136be615a08b0eb569a59760f05fcc7290
Jakob Calero <jakob.calero@gmail.com>
2012-10-01 12:02:25 +0000

    Deleted root files that are now in the src/ folder

--- readIR.m
+++
@@ -1,7 +0,0 @@
-function sensorVals = readIR(s)
-fprintf(s,'N');
-pause(0.001);
-sensorString = fscanf(s);
-splitString = regexp(sensorString,',','split');
-sensorVals = cellfun(@str2num,splitString(2:end));
-end
