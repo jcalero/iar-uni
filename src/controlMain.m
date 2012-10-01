79d564f15701141d173ac9ff088853777a4ad09e
Samuel Neugber <yogurt.der.weise@gmail.com>
2012-10-01 14:57:57 +0000

    Now maps the path it traversed by odometry. Also some trash code I was trying to add to catch "Ctrl+C" but couldn't get to work. //Jakob

--- src/controlMain.m
+++ src/controlMain.m
@@ -1,5 +1,5 @@
-function controlMain(s, speed)
-    turnspeed = round(speed/2);
+function positions = controlMain(s, speed)
+    turnspeed = round(speed/3);
     
     setCounts(s,0,0);
     oldWheelCountsL = 0;
@@ -7,10 +7,23 @@ function controlMain(s, speed)
     angle = 0;
     xPos = 0;
     yPos = 0;
+    
+    positions = [xPos,yPos];
+    
+    direction = 's';
+    turning = false;
+    
+    iteration = 0;
+    
+    c = onCleanup(@() cleanupfun(positions));
 
-    while (true)
+    while (iteration < 500)
+    
+    	pause(0.05);
+        iteration = iteration +1;
+        
         % read sensor values
-        ir = readIR(s);
+        ir = readIR(s)
         % rename individual sensors
         leftIR1 = ir(1);
         leftIR2 = ir(2);
@@ -29,7 +42,7 @@ function controlMain(s, speed)
         
         % odo 2: calculate angle
      	% φ ← φ + Δφ = φ - 0.5*(vleft - vright)/(widthfactor)
-     	% 2 x 40mm radius * 0.08mm per wheelcount != "width factor" of 330, which I just found through trial and error
+     	% 2 x 40mm radius * 0.08mm per wheelcount != 'width factor' of 330, which I just found through trial and error
      	angle = angle - 0.5*(wheelDeltaL - wheelDeltaR)/330;
      	
      	if(angle > 2*pi) angle = angle - 2*pi; end
@@ -39,40 +52,60 @@ function controlMain(s, speed)
         % odo 3: calculate position
         % x ← x + Δx =x + 0.5*(vleft + vright) cos(φ)
 		% y ← y + Δy =y + 0.5*(vleft + vright) sin(φ)
-		xPos = xPos + 0.5*(wheelDeltaL + wheelDeltaR)*0.08 * cos(angle)
-		yPos = yPos + 0.5*(wheelDeltaL + wheelDeltaR)*0.08 * sin(angle)
+		xPos = xPos + 0.5*(wheelDeltaL + wheelDeltaR)*0.08 * cos(angle);
+		yPos = yPos + 0.5*(wheelDeltaL + wheelDeltaR)*0.08 * sin(angle);
+        
+        positions = cat(1,positions,[xPos,yPos]);
 		
+       % if(~turning)
+            % Adjust Left based on centre sensor
+            if ((midRightIR > 150 || rightIR1 > 450 || rightIR2 > 600))
+                stop(s)
+                turn(s,-turnspeed,turnspeed)
+                direction = 'l';
+                turning = true;
+                continue
+            end
 
-        % Adjust Left based on centre sensor
-        if (midRightIR > 300 || rightIR1 > 500)
-            stop(s)
-            turn(s,-turnspeed,turnspeed)
-            continue
-        end
+            % Adjust Left based on centre sensor
+            if ((midLeftIR > 150 || leftIR1 > 450 || leftIR2 > 600))
+                stop(s)
+                turn(s,turnspeed,-turnspeed)
+                direction = 'r';
+                turning = true;
+                continue
+            end
 
-        % Adjust Left based on centre sensor
-        if (midLeftIR > 300 || leftIR1 > 500)
-            stop(s)
-            turn(s,turnspeed,-turnspeed)
-            continue
-        end
+            % Wall re allignment
+            if (leftIR1 < 250 && leftIR1 > 180)
+                stop(s)
+                turn(s,-turnspeed,turnspeed)
+                direction = 'l';
+                turning = true;
+                continue
+            end
 
-        % Wall re allignment
-        if (leftIR1 < 250 && leftIR1 > 180)
-            stop(s)
-            turn(s,-turnspeed,turnspeed)
-            continue
-        end
+            if (rightIR1 < 250 && rightIR1 > 180)
+                stop(s)
+                turn(s,turnspeed,-turnspeed)
+                direction = 'r';
+                turning = true;
+                continue
+            end
+       % end
 
-        if (rightIR1 < 250 && rightIR1 > 180)
-            stop(s)
-            turn(s,turnspeed,-turnspeed)
-            continue
-        end
-
-        go(s,speed);
-
-        pause(0.05);
+        % if (direction ~= 'f') 
+        go(s,speed); 
+        %end
+        turning = false;
+        direction = 'f';
+        
     end
+    
+    function p = cleanupfun(positions)
+        fprintf('test');
+        p = positions
+    end
+    
 end
 
