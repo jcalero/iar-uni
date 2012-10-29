c764d954151b6048b9bb79edd59f204096d52b4e
Samuel Neugber <yogurt.der.weise@gmail.com>
2012-10-29 14:56:30 +0000

    Got the reasoning behind P(k + 1|k), needs to be initialized somewhere else though

--- src/ekf.m
+++ src/ekf.m
@@ -18,10 +18,14 @@ if(odoAngle > 2*pi) odoAngle = odoAngle - 2*pi; end
 if(odoAngle < 0) odoAngle = odoAngle + 2*pi; end
 
 jacobianMatrix = [(-odoDist * sin(odoAngle)) cos(2*angle);(odoDist * cos(odoAngle)) sin(2*angle); 1 0];
+jacobianMatrixDash = [1 0 (-odoDist * sin(odoAngle)); 0 1 (odoDist * cos(odoAngle)); 0 0 1];
 
 noiseMatrix = [((angle^2)*rotationalNoise) 0; 0 (translationalNoise)];
 
 noiseCovMatrix = jacobianMatrix * noiseMatrix * transpose(jacobianMatrix);
+
+oldErrorCovMatrix = ones(3,3); % this still needs to be initialized properly somewhere at the first timestep. just here for testing purposes
+errorCovMatrix = jacobianMatrixDash * oldErrorCovMatrix * transpose(jacobianMatrixDash) + noiseCovMatrix
 
 % stuck at trying to calculate P(k + 1|k), which is defined using itself...
 
