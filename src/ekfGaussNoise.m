4d3740b872a9dd61c9f9a614d569dd5016237cc3
Samuel Neugber <yogurt.der.weise@gmail.com>
2012-11-03 20:31:40 +0000

    skeleton functions needed for kalman filter (still have to be implemented apart from ekfDirectMeasure)

---
+++ src/ekfGaussNoise.m
@@ -0,0 +1,7 @@
+function [ output_args ] = ekfGaussNoise( input_args )
+%EKFGAUSSNOISE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ src/ekfJacobianMeasure.m
@@ -0,0 +1,7 @@
+function [ output_args ] = ekfJacobianMeasure( input_args )
+%EKFJACOBIANMEASURE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
