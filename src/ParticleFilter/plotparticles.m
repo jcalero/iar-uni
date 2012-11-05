7f72017aaf9920fd5e5e72217c896196b3334fe8
Jakob Calero <jakob.calero@gmail.com>
2012-11-05 15:47:44 +0000

    Deleted old, disorganised files. Moved the particle field code to the simulation.

--- src/ParticleFilter/plotparticles.m
+++
@@ -1,8 +0,0 @@
-[numpart unused]= size(particles.position);
-plotmap
-
-for j=1:numpart
-    plot(particles.position(j,1),particles.position(j,2),'k+')
-    direc=[particles.position(j,:);particles.position(j,:)+particles.direction(j,:)*50];
-    plot(direc(:,1),direc(:,2),'c');
-end
