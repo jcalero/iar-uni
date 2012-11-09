9461fd3bee41aa1e37fbfc1638b49e664b312a97
Samuel Neugber <yogurt.der.weise@gmail.com>
2012-11-09 16:57:56 +0000

    attempt at putting the old control into our Run script

--- src/Task3/real/Run.m
+++ src/Task3/real/Run.m
@@ -6,6 +6,9 @@ stepmove.speed=0;    % speed of movement (distance to move at every iteration)
 stepmove.turn=0;     % angle to turn at every iteration
 load realmap
 load distToIRMap
+
+goals = [];
+
 [particles, robot]=initialize(numparticles);    % initializes particles and the robot
 
 oldrobot.position = robot.position;
@@ -39,7 +42,15 @@ oldWheelCounts = [0 0];
 go(s, 2);
 % ===============
 
+speed = 2;
+turnspeed = speed/2;
+lSpeed = speed;
+rSpeed = speed;
+oldLSpeed = speed;
+oldRSpeed = speed;
+
 while true
+    
     % Generate new robot position and stepmove from odometry
     [ robot, stepmove, oldWheelCounts ] = odometry(s, oldrobot, oldWheelCounts);
     
@@ -68,7 +79,55 @@ while true
     plotall(robot, particles, map, bestPose);
     
     % Do navigation & control here.
-    % 
+    %
+    
+    foundFood = 0;
+    [~,indeces] = size(find(sensor > 30));
+    if indeces > 0
+       foundFood = 1; 
+    end
+    
+    if foundFood && find(ismember(goals,robot.position)) > 0
+        goals = [goals robot.position];
+        stop(s);
+        continue
+    end
+    
+    [a,c] = getPotFieldVec(robot.position,robot.direction,realMap,goals);
+    
+    if((rightIR1 > 250 || rightIR2 > 400) && (leftIR1 > 250 || leftIR2 > 400) && ((midRightIR + midLeftIR)/2 < 150))
+        setSpeeds(s,speed);
+        continue;
+    end
+    
+    % Adjust Left based on centre sensor
+    if (midRightIR > 150 || rightIR1 > 250 || rightIR2 > 400)
+        stop(s)
+        turn(s,-turnspeed,turnspeed)
+        continue
+    end
+
+    % Adjust Left based on centre sensor
+    if ((midLeftIR > 150 || leftIR1 > 250 || leftIR2 > 400))
+        stop(s)
+        turn(s,turnspeed,-turnspeed)
+        continue
+    end
+    
+    if(a > 0)
+        rSpeed = speed;
+        lSpeed = max(round(speed - (12 * abs(a) / 180) -1),-speed);
+    else
+        lSpeed = speed;
+        rSpeed = max(round(speed - (12 * abs(a) / 180) -1),-speed);
+    end
+    
+    
+    if(oldLSpeed ~= lSpeed || oldRSpeed ~= rSpeed)
+         setSpeeds(s,lSpeed,rSpeed);
+         oldLSpeed = lSpeed;
+         oldRSpeed = rSpeed;
+    end
 
     fprintf('Paused. press any key for next iteration.\nPress ctrl+C to stop \n')
     pause(0.01);
